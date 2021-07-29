f3eafe02473de442332d46d874354d8bf82bed85
Shoumik Dhar <85100168+Shoumik-WebTech@users.noreply.github.com>
2021-07-29 16:45:11 +0600

    Update DbConnect.php

--- DbConnect.php
+++ DbConnect.php
@@ -3,7 +3,7 @@
 
 function connection()
 {
-    $connection =new mysqli("localhost","Jahidul","1234","wtk");
+    $connection =new mysqli("localhost","Shoumik","10203040","WBT");
     if($connection->connect_errno)
     {
         die("Database connection failed.".$connection->connect_error);
